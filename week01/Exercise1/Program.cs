05dd782a50a1d1a81aa8efe2e5e905ca8aed27ce
Uinty <usantana@byupathway.edu>
2026-03-06 23:03:05 +0100

    prep 3 finished

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,15 +1,9 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        //ask the user for their first and last name, then greet them with their full name in the format "LastName, FirstName LastName"
-        Console.Write("what is your first name? ");
-        string firstName = Console.ReadLine();
-        Console.Write("what is your last name? ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
-    }
-}
+//ask the user for their first and last name, then greet them with their full name in the format "LastName, FirstName LastName"
+Console.Write("what is your first name? ");
+string firstName = Console.ReadLine();
+Console.Write("what is your last name? ");
+string lastName = Console.ReadLine();
+Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
 
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,67 +1,61 @@
 using System;
 
-class Program
+Console.Write("Enter your grade percentage: ");
+string input = Console.ReadLine();
+int grade = int.Parse(input);
+
+string letter = "";
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.Write("Enter your grade percentage: ");
-        string input = Console.ReadLine();
-        int grade = int.Parse(input);
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
 
-        string letter = "";
+int lastDigit = grade % 10;
+string sign = "";
 
-        if (grade >= 90)
-        {
-            letter = "A";
-        }
-        else if (grade >= 80)
-        {
-            letter = "B";
-        }
-        else if (grade >= 70)
-        {
-            letter = "C";
-        }
-        else if (grade >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
+if (lastDigit >= 7)
+{
+    sign = "+";
+}
+else if (lastDigit < 3)
+{
+    sign = "-";
+}
 
-        int lastDigit = grade % 10;
-        string sign = "";
+if (letter == "A" && sign == "+")
+{
+    sign = "";
+}
 
-        if (lastDigit >= 7)
-        {
-            sign = "+";
-        }
-        else if (lastDigit < 3)
-        {
-            sign = "-";
-        }
+if (letter == "F")
+{
+    sign = "";
+}
 
-        if (letter == "A" && sign == "+")
-        {
-            sign = "";
-        }
+Console.WriteLine($"Your grade is {letter}{sign}");
 
-        if (letter == "F")
-        {
-            sign = "";
-        }
-
-        Console.WriteLine($"Your grade is {letter}{sign}");
-
-        if (grade >= 70)
-        {
-            Console.WriteLine("Congratulations! You passed the course.");
-        }
-        else
-        {
-            Console.WriteLine("Don't give up! Try again next time.");
-        }
-    }
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations! You passed the course.");
+}
+else
+{
+    Console.WriteLine("Don't give up! Try again next time.");
 }
