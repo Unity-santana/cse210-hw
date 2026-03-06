4c1c7cfe01dd4ab62ad7277ec1e4c050ebe45a70
Uinty <usantana@byupathway.edu>
2026-03-06 23:02:33 +0100

    prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Create a random number generator
+        Random randomGenerator = new Random();
+
+        // Generate the magic number between 1 and 100
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1; // variable to store the user's guess
+
+        // Keep looping until the user guesses correctly
+        while (guess != magicNumber)
+        {
+            // Ask the user for their guess
+            Console.Write("What is your guess? ");
+
+            // Read the guess and convert it to an integer
+            guess = int.Parse(Console.ReadLine());
+
+            // Check if the guess is lower than the magic number
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            // Check if the guess is higher than the magic number
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            // If neither condition above happens, the guess is correct
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
