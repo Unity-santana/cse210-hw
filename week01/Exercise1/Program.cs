b773b2e5337628d76ea6815ea08c5b00971a523e
Uinty <usantana@byupathway.edu>
2026-03-06 12:48:43 +0100

    finished prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is you first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("what is you last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
