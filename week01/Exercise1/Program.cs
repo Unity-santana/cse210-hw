9fcb2eb31cf4fb1ddaf28b9986b8329fc15a0cd2
Uinty <usantana@byupathway.edu>
2026-03-06 13:31:28 +0100

    added comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,11 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        Console.Write("what is you first name? ");
+        //ask the user for their first and last name, then greet them with their full name in the format "LastName, FirstName LastName"
+        Console.Write("what is your first name? ");
         string firstName = Console.ReadLine();
-        Console.Write("what is you last name? ");
+        Console.Write("what is your last name? ");
         string lastName = Console.ReadLine();
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
+
