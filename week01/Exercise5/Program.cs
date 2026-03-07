625d6bd30176f730ed8a20a2c10d717c94d0f608
Uinty <usantana@byupathway.edu>
2026-03-07 11:10:05 +0100

    Prep 5 finished

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,3 +1,57 @@
 using System;
 
-Console.WriteLine("Hello World! This is the Exercise5 Project.");
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Call the welcome function
+        DisplayWelcome();
+
+        // Get the user's name
+        string userName = PromptUserName();
+
+        // Get the user's favorite number
+        int userNumber = PromptUserNumber();
+
+        // Square the number
+        int squaredNumber = SquareNumber(userNumber);
+
+        // Display the result
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function to display welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function to ask for the user's name and return it
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Function to ask for the user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Function to square the number
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    // Function to display the result
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+}
