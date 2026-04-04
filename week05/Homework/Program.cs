5c1da3d2d29895970ccdf2b8aa0fb2d408461a03
Uinty <usantana@byupathway.edu>
2026-04-04 16:19:50 +0100

    Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,1 +1,19 @@
-Console.WriteLine("Hello World! This is the Homework Project.");
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+    }
+}
