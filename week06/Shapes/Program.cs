322dd8189fe5b0725d928a176f9ec0b21170060d
Uinty <usantana@byupathway.edu>
2026-04-09 23:50:34 +0100

    shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,1 +1,24 @@
-Console.WriteLine("Hello World! This is the Shapes Project.");
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Loop through shapes
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+    }
+}
